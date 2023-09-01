9a00bb0820a0f9d767335a8ef372beb7b8c9fe9b
safa <fhamidaakterj@gmail.com>
2023-09-01 21:27:31 +0900

    Commit 02

--- tests/test_login_page_negative.py
+++ tests/test_login_page_negative.py
@@ -11,8 +11,35 @@ class TestNegativeScenarios:
 
     @pytest.mark.login
     @pytest.mark.negative
-    def test_negative_username(self, driver):
+    @pytest.mark.parametrize("username, password, expected_error_message",
+                             [("incorrectUser", "Password123", "Your username is invalid!"),
+                              ("student", "incorrectPassword", "Your password is invalid!")])
+    def test_negative_login(self, driver, username, password, expected_error_message):
+        # Go to webpage
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+        # Type username incorrectUser into Username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys(username)
+        # username_locator.send_keys("student")
 
+        # Type password Password123 into Password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys(password)
+
+        # Punch Submit button
+        submit_btn_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+        submit_btn_locator.click()
+        time.sleep(2)
+
+        # Verify error message is displayed
+        error_message_locator = driver.find_element(By.ID, "error")
+        assert error_message_locator.is_displayed(), "error message is not displayed but it should "
+
+        # Verify error message  text is Your username is invalid!
+        error_message = error_message_locator.text
+        assert error_message == expected_error_message, "Error message is not expected"
+
+    def test_negative_username(self, driver):
         # Go to webpage
         driver.get("https://practicetestautomation.com/practice-test-login/")
         # Type username incorrectUser into Username field
@@ -37,8 +64,6 @@ class TestNegativeScenarios:
         error_message = error_message_locator.text
         assert error_message == "Your username is invalid!", "Error message is not expected"
 
-    @pytest.mark.login
-    @pytest.mark.negative
     def test_negative_password(self, driver):
         # Open page
 
@@ -63,7 +88,7 @@ class TestNegativeScenarios:
 
         # Verify error message is displayed
         error_message_locator = driver.find_element(By.ID, "error")
-        assert error_message_locator.is_displayed(), "error message is not displayed bur it should "
+        assert error_message_locator.is_displayed(), "error message is not displayed but it should "
 
         # Verify error message  text is Your password is invalid!
         error_message = error_message_locator.text
